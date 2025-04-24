3bd43dc4eea15774c0d5fdf44c41f20318e9e1f0
AhmadAlali1212 <aa2994672@gmail.com>
2025-04-24 21:35:50 +0300

    create not view body

---
+++ lib/views/notes_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/custom_note_item.dart';
+import 'package:notes_app/views/custom_app_bar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        CustomAppBar(),
+        CustomNoteItem(),
+      ],
+    );
+  }
+}
